d52c386f30b247e544f896f36fa2e5dad94e50da
krowker <krowker97@gmail.com>
2021-10-07 01:14:20 +0300

    kostili

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState} from 'react'
 import './App.css';
 import key from './key.js';
 
 function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState({})
   const [lat, setLat] = useState();
   const [lon, setLong] = useState();
+  // const [error, setError] = useState(null);
+  // const [isLoaded, setIsLoaded] = useState(false);
   const apiKey = key;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
@@ -16,22 +18,23 @@ function App() {
     });
 
     fetch(apiUrl)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setData(data)
-        console.log(data);
-      });
-
-  }, [lat, lon]);
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setData(data)
+    });
+    
+    console.log(data);
+  }, [lat, lon])  
 
   return (
     <div className="App">
-      <p>Город:{data.name}</p>
-      <p>Температура:{data.main.temp}</p>
+      <p>Город: {data.name}</p>
+      <p>Температура: {
+        data.main === undefined ? <span>error</span> : <span>{data.main.temp}</span>
+      }</p>
     </div>
   );
 }
-
 export default App;
