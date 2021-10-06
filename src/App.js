2be7a0731e308494ccaec8c658d10b4cca867045
krowker <krowker97@gmail.com>
2021-10-06 14:07:52 +0300

    add key.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import './App.css';
+import key from './key.js';
 
 function App() {
   const [data, setData] = useState([])
   const [lat, setLat] = useState();
   const [long, setLong] = useState();
-  const apiKey = "6f3e620af43ddfebe8ad93327e8f2b6e";
+  const apiKey = key;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
 
   useEffect(() => {
@@ -24,15 +25,6 @@ function App() {
       });
 
   }, [lat, long]);
-
-  // fetch(apiUrl)
-  // .then((response) => {
-  //   return response.json();
-  // })
-  // .then((data) => {
-  //   setData(data)
-  //   console.log(data);
-  // });
 
   return (
     <div className="App">
