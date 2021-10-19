df3da8b735268aaf82f866768312b75b06ba373a
krowker <krowker97@gmail.com>
2021-10-19 22:27:17 +0300

    edit useLoc

--- src/components/useLocation.js
+++ src/components/useLocation.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
-export const useLocation=()=>{
+export const useLocation = () => {
    const [lat, setLat] = useState();
    const [lon, setLong] = useState();
-   
+
    useEffect(() => {
       navigator.geolocation.getCurrentPosition(function (position) {
-            setLat(position.coords.latitude);
-         setLong(position.coords.longitude);       
+         setLat(position.coords.latitude);
+         setLong(position.coords.longitude);
       });
    }, [])
    return [lat, lon]
